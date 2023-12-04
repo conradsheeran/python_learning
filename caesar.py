0609b1144615efc0b567aff77442308de4614411
conradsheeran <conradsheeran2005@gmail.com>
2023-12-04 20:20:41 +0800

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -7,12 +7,12 @@ pwd = []  # 为保证重复字符也得以加密
 for letter in raw_massage_list:  # 运行加密算法
     if letter.isalpha():
         if letter.isupper():  # 处理大写字母无法加密问题
-            letter = letter.lower()
+            letter_lower = letter.lower()
             index_rml = raw_massage_list.index(letter)
-            index_alphabet = alphabet.index(letter)
+            index_alphabet = alphabet.index(letter_lower)
             pwd_index = index_alphabet + 2  # 使用索引位移实现字符位移
-            pwd_letter = alphabet[pwd_index]
-            raw_massage_list[index_rml] = pwd_letter.upper()
+            pwd_letter = alphabet[pwd_index].upper()
+            raw_massage_list[index_rml] = pwd_letter
         else:
             index_rml = raw_massage_list.index(letter)
             index_alphabet = alphabet.index(letter)
