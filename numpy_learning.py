0ff0ca7a4575de0565b673e0cb60e498ec4278a8
conradsheeran <conradsheeran2005@gmail.com>
2024-05-09 23:34:12 +0800

    [Update] Learning NumPy

--- numpy_learning.py
+++ numpy_learning.py
@@ -1,6 +1,17 @@
-import numpy as np
 if __name__ == '__main__':
     import numpy as np
-    arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-    print(arr[0, 1:4])
+    import matplotlib.pyplot as plt
+
+    arr = np.array(([1, 0, 3, 4, 5], [6, 7, 8, 9, 10], [1, 0, 0, 0, 0], [2, 0, 0, 0, 0]), dtype="i4")
+    new_arr = arr.astype("bool")
+    n_arr = arr.reshape(2, 5, -1)
+    arr_one = np.ones((2, 5), dtype=int)
+    print(arr)
+    print(arr[2, 1:4])
+    print(new_arr)
+    print(n_arr)
+    print(arr_one)
     print(arr.dtype)
+    print(new_arr.dtype)
+    print(arr.shape)
+    print(arr.cumsum())
